0a1cc4a43157326ceb75714cbe94004a775b43fa
theProgrammerOfOurTime <azarova.v2104@gmail.com>
2024-10-21 20:12:38 +0300

    v2.0

--- Boltenkov_Stanislav_lab1/filterscv.py
+++ Boltenkov_Stanislav_lab1/filterscv.py
@@ -1,98 +1,58 @@
 import cv2 as cv
 import numpy as np
 
-def conversionToGray(imagePath):
-    img = cv.imread(imagePath, -1)
-    
-    grayIm = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
-    
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            grayIm[i][j] = np.uint8(0.299 * img[i][j][2] + 0.587 * img[i][j][1] + 0.114 * img[i][j][0])#BGR
-            
-         
-    cv.imshow("conversionToGray", grayIm)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-    return
+def conversionToGray(img):
+  
+    blue, green, red, = np.split(img, 3, axis = 2)
+    return (0.114 * red +  0.587 * green + 0.299 * blue).astype("uint8")
 
-def resizeImage(imagePath, kheight, kwidth):
-     img = cv.imread(imagePath, -1)
+def resizeImage(img, kheight, kwidth):
+    
      nheight = int(img.shape[0] * kheight)
      nwidth = int(img.shape[1] * kwidth)
-     newIm = np.zeros((nheight, nwidth, 3), np.uint8)
      
+     newIm = np.zeros((nheight, nwidth, img.shape[2]), np.uint8)   
+       
      k1 = img.shape[0] / nheight
      k2 = img.shape[1] / nwidth
      
      for i in range(nheight):
         for j in range(nwidth):
             newIm[i][j] = img[int(i * k1)][int(j * k2)]   
-            
-     cv.imshow("resizeImage", newIm)
-     cv.waitKey(0)
-     cv.destroyAllWindows()
-     return
+     return newIm
 
-def sepiaPhotoEffect(imagePath):
-    img = cv.imread(imagePath, -1)
+def sepiaPhotoEffect(img):
+  
+    blue, green, red = np.split(img, 3, axis = 2)
+    newBlue = np.clip(0.272 * red + 0.534 * green + 0.131 * blue, 0, 255)
+    newGreen = np.clip(0.349 * red + 0.686 * green + 0.168 * blue, 0, 255)
+    newRed = np.clip(0.393 * red + 0.769 * green + 0.189 * blue, 0, 255)
     
-    newIm = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
-    
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            newIm[i][j][0] = min(0.272 * img[i][j][2] + 0.534 * img[i][j][1] + 0.131 * img[i][j][0], 255) 
-            newIm[i][j][1] = min(0.349 * img[i][j][2] + 0.686 * img[i][j][1] + 0.168 * img[i][j][0], 255)
-            newIm[i][j][2] = min(0.393 * img[i][j][2] + 0.769 * img[i][j][1] + 0.189 * img[i][j][0], 255)
-            
-    cv.imshow("sepiaPhotoEffect", newIm)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-    return
+    return np.concatenate([newBlue, newGreen, newRed], axis = 2).astype("uint8")
+   
 
-def vignettePhotoEffect(imagePath):
-    img = cv.imread(imagePath, -1)
-    
-    newIm = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
-    
-    centerx = img.shape[0] // 2
-    centery = img.shape[1] // 2
-    r = min(centerx, centery) + (max(centerx, centery) - min(centerx, centery)) // 2
-    r = r * r
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            dist = (centerx - i) * (centerx - i) + (centery - j) * (centery - j)
-            newIm[i][j] = img[i][j]
-            if (dist > r):
-                newIm[i][j] = pow(r / dist, 3) * img[i][j]
-            
-    cv.imshow("vignettePhotoEffect", newIm)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-    
-    return
+def vignettePhotoEffect(img, r):
 
-def pixelation(imagePath, x0, y0, x1, y1):
-    img = cv.imread(imagePath, -1)
-    
     newIm = img
+    r = r * r
     
-    if (y0 > y1 or x0 > x1):
-        y0, y1, x0, x1 = y1, y0, x1, x0
-    
-    countAreaX = (x1 - x0 + 9) // 10
-    countAreaY = (y1 - y0 + 9) // 10
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            dist = (img.shape[0] // 2 - i) ** 2 + (img.shape[1] // 2 - j) ** 2
+            if (dist > r):
+                newIm[i][j] = pow(r / dist, 3) *  newIm[i][j]
+    return newIm
+
+def pixelation(img, x0, y0, x1, y1, pix):
+
+    newIm = img
+   
+    countAreaX = (x1 - x0 + pix - 1) // pix
+    countAreaY = (y1 - y0 + pix - 1) // pix
     
     for i in range(countAreaX):
         for j in range(countAreaY):
-            stx = x0 + i * 10
-            enx = min(x0 + 10 * (i + 1), x1)
-            sty = y0 + j * 10
-            eny = min(y0 + 10 * (j + 1), y1)
-            newIm[stx : enx, sty : eny] = np.mean(np.mean(img[stx : enx, sty : eny], axis = 0), axis = 0)
-        
-    cv.imshow("pixelation", newIm)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-    
-    return
+            stx, enx = x0 + i * pix, min(x0 + pix * (i + 1), x1)
+            sty, eny = y0 + j * pix, min(y0 + pix * (j + 1), y1)
+            newIm[stx : enx, sty : eny] = np.mean(np.mean(img[stx : enx, sty : eny], axis = 0), axis = 0) 
+    return newIm
--- Boltenkov_Stanislav_lab1/main.py
+++ Boltenkov_Stanislav_lab1/main.py
@@ -1,19 +1,75 @@
 import filterscv
+import cv2 as cv
+import numpy as np
+import argparse as arg
+
+def argumentParse():
+     parser = arg.ArgumentParser()
+     
+     parser.add_argument(
+         "-p", "--path",
+         help = "Path to an image",
+         required = True
+     )
+     parser.add_argument(
+         "-m", "--mode",
+         type = int,
+         help = "mode process image (0, 1, 2, 3, 4)",
+         required = True
+     )
+     parser.add_argument(
+         "-kx",
+         type = float,
+         help = "coeficent x for resize image"
+     ) 
+     parser.add_argument(
+         "-ky",
+         type = float,
+         help = "coeficent y for resize image"
+     )
+     parser.add_argument(
+         "-r", "--radius",
+         type = int,
+         help = "radius for vignette photo effect"
+     )
+     parser.add_argument(
+         "-c", "--coordinates",
+         type = int,
+         nargs= 4,
+         help = "rectangle coordinates for pixelation"
+     )
+     parser.add_argument(
+         "-px",
+         type = int,
+         help = "pixel size for pixelation"
+     )
+     
+     args = parser.parse_args()
+     return args
+     
 
 def main():
-    imagePath = input()
-    numOperations = int(input())
-    if (numOperations == 0):
-        filterscv.conversionToGray(imagePath)
-    elif (numOperations == 1):
-        kx, ky = [float(i) for i in (input().split())]
-        filterscv.resizeImage(imagePath, kx, ky)
-    elif (numOperations == 2):
-        filterscv.sepiaPhotoEffect(imagePath)
-    elif (numOperations == 3):
-        filterscv.vignettePhotoEffect(imagePath)
-    elif (numOperations == 4):
-        x0, y0, x1, y1 = [int(i) for i in (input().split())]
-        filterscv.pixelation(imagePath, x0, y0, x1, y1)
+    
+    args = argumentParse()
+    img = cv.imread(args.path, -1)
+
+    if (args.mode == 0):
+        cv.imshow("conversionToGray", filterscv.conversionToGray(img))
+        
+    elif (args.mode == 1):
+        
+        cv.imshow("resizeImage", filterscv.resizeImage(img, args.kx, args.ky))
+        
+    elif (args.mode == 2):
+        cv.imshow("sepiaPhotoEffect", filterscv.sepiaPhotoEffect(img))
+        
+    elif (args.mode == 3):
+        cv.imshow("vignettePhotoEffect", filterscv.vignettePhotoEffect(img, args.radius))
+        
+    elif (args.mode == 4):
+        cv.imshow("pixelation", filterscv.pixelation(img, args.coordinates[0], args.coordinates[1], args.coordinates[2], args.coordinates[3], args.px))
+        
+    cv.waitKey(0)
+    cv.destroyAllWindows()
         
 main()
