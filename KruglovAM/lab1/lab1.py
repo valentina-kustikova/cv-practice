4807f60e55de0db28af5f64986a19e8e4e5bc2df
KrYack <x.kryack.x@gmail.com>
2024-10-25 01:01:08 +0300

    new changes

--- KruglovAM/lab1/lab1.py
+++ KruglovAM/lab1/lab1.py
@@ -40,7 +40,7 @@ def cli_argument_parser():
     parser.add_argument('-r', '--radius',
                         help='Radius for vignette filter',
                         type=int,
-                        default=1,
+                        default=200,
                         dest='radius')
 
     args = parser.parse_args()
