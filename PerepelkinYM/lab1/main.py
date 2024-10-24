62760986e7111cd9b38f73fc888944e10d6e6683
TepidmishA <perep.main@mail.ru>
2024-10-24 19:07:03 +0300

    Add argparse

--- PerepelkinYM/lab1/main.py
+++ PerepelkinYM/lab1/main.py
@@ -1,5 +1,72 @@
 import numpy as np
 import cv2 as cv
+import argparse
+
+
+def cli_arguments():
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument('--image', '-i',
+						help='Image path',
+						type=str,
+						dest='image_path',
+						required=False,
+						default="image.png")
+
+	parser.add_argument("--filter", "-f",
+						help="Filter type (grey, resize, sepia, vignette, pixelate)",
+						type=str,
+						dest="filter_type",
+						required=False,
+						default="pixelate",
+						choices=["grey", "resize", "sepia", "vignette", "pixelate"])
+
+	# Аргументы для фильтра resize
+	parser.add_argument("--width", "-w",
+						help="Width for resizing",
+						type=int,
+						dest="width",
+						required=False,
+						default=1920)
+
+	parser.add_argument("--height", "-ht",
+						help="Height for resizing",
+						type=int,
+						dest="height",
+						required=False,
+						default=1080)
+
+	# Аргумент для фильтра vignette
+	parser.add_argument("--radius", "-r",
+						help="Radius for vignette",
+						type=int,
+						dest="radius",
+						required=False,
+						default=80)
+
+	# Аргумент для фильтра pixelate
+	parser.add_argument("--pixel_size", "-ps",
+						help="Pixel size for pixelate",
+						type=int,
+						dest="pixel_size",
+						required=False,
+						default=10)
+
+	return parser.parse_args()
+
+
+def image_grey(img):
+	"""
+	Функция перевода изображения в оттенки серого
+
+	:param img: Исходное изображение
+	:return: Изменённое изображение в оттенках серого
+	"""
+
+	weights = np.array([0.1140, 0.5870, 0.2989])                # Параметры инвертированы, т.к. opencv хранит изображения в BGR
+	res_img = np.dot(img[:, :, :3], weights).astype(np.uint8)   # img[..., :3] = img[:,:, :3] - игнорируем альфа канал
+
+	return res_img
 
 
 def image_change_resolution(img, new_width, new_height):
@@ -12,6 +79,8 @@ def image_change_resolution(img, new_width, new_height):
 	:return: Изменённое изображение с новым разрешением
 	"""
 
+	if new_width <= 0 or new_height <= 0:
+		raise ValueError("Invalid width or height.")
 	old_height, old_width = img.shape[:2]
 
 	# Коэффициенты масштабирования
@@ -25,20 +94,6 @@ def image_change_resolution(img, new_width, new_height):
 	# Индексация с использованием массивов в numpy
 	# img[[0, 2, 4], [0, 2, 4]] -> выборка пикселей с координатами (0,0), (0,2), (0,4), (2,0), (2,2), (2,4), (4,0), (4,2), (4,4)
 	res_img = img[y_indices[:, None], x_indices]
-
-	return res_img
-
-
-def image_grey(img):
-	"""
-	Функция перевода изображения в оттенки серого
-
-	:param img: Исходное изображение
-	:return: Изменённое изображение в оттенках серого
-	"""
-
-	weights = np.array([0.1140, 0.5870, 0.2989])                # Параметры инвертированы, т.к. opencv хранит изображения в BGR
-	res_img = np.dot(img[:, :, :3], weights).astype(np.uint8)   # img[..., :3] = img[:,:, :3] - игнорируем альфа канал
 
 	return res_img
 
@@ -72,7 +127,7 @@ def image_vignette_rad(img, rad):
 	"""
 
 	if rad <= 0:
-		raise ValueError("Invalid radius")
+		raise ValueError("Invalid radius.")
 
 	# Размеры изображения и центр
 	rows, cols = img.shape[:2]
@@ -94,7 +149,7 @@ def image_vignette_rad(img, rad):
 	return res_img
 
 
-def image_pixelite(img, win_name, pixel_size):
+def image_pixelate(img, win_name, pixel_size):
 	"""
 	Функция пикселизации выделенной прямоугольной области изображения
 
@@ -104,6 +159,9 @@ def image_pixelite(img, win_name, pixel_size):
 	:return: Изменённое изображение с эффектом пикселизации выделенной прямоугольной области
 	:raise ValueError("Invalid selected area"): Выделенная область слишком мала
 	"""
+
+	if pixel_size <= 0:
+		raise ValueError("Invalid pixel size.")
 
 	# Обрезаем изображение
 	coords = cv.selectROI(win_name, img, showCrosshair=False, printNotice=False)
@@ -115,7 +173,7 @@ def image_pixelite(img, win_name, pixel_size):
 	new_w = (cropped_image.shape[1] // pixel_size) * pixel_size
 
 	if new_h == 0 or new_w == 0:
-		raise ValueError("Invalid selected area")
+		raise ValueError("Invalid selected area.")
 
 	# Разбиваем исходное изображение на блоки размером [pixel_size, pixel_size]
 	# list [блоки по верт, pixel_size, блоки по гориз., pixel_size, 3]
@@ -131,14 +189,30 @@ def image_pixelite(img, win_name, pixel_size):
 
 
 if __name__ == '__main__':
-	img_cv = cv.imread("image.png")
-	img_cv = cv.resize(img_cv, [int(x / 2) for x in img_cv.shape[:2][::-1]])
+	args = cli_arguments()
 
-	# new_image = image_change_resolution(img_cv, 1920, 1080)
-	# new_image = image_grey(img_cv)
-	# new_image = image_sepia(img_cv)
-	# new_image = image_vignette_rad(img_cv, 80)
-	new_image = image_pixelite(img_cv, "Image", 10)
+	image_path = args.image_path
+	input_image = cv.imread(image_path)
 
-	cv.imshow("Image", new_image)
+	if input_image is None:
+		raise ValueError("Image not found or unable to load.")
+
+	input_image = cv.resize(input_image, [int(x / 2) for x in input_image.shape[:2][::-1]])
+
+	cv.imshow("Original", input_image)
+
+	filter_type = args.filter_type
+	match filter_type:
+		case "grey":
+			output_image = image_grey(input_image)
+		case "resize":
+			output_image = image_change_resolution(input_image, args.width, args.height)
+		case "sepia":
+			output_image = image_sepia(input_image)
+		case "vignette":
+			output_image = image_vignette_rad(input_image, args.radius)
+		case "pixelate":
+			output_image = image_pixelate(input_image, "Original", args.pixel_size)
+
+	cv.imshow("Image", output_image)
 	cv.waitKey(0)
