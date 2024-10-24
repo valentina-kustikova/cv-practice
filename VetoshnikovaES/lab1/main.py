c7d0d8a77acf6131e7bd3f18b7e301797d51ae75
Veto1234-ii <vetoshnikova14@bk.ru>
2024-10-25 00:20:15 +0300

    add files

---
+++ VetoshnikovaES/lab1/main.py
@@ -0,0 +1,253 @@
+import argparse
+import sys
+import cv2 as cv
+import numpy as np
+
+
+def cli_argument_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-m', '--mode',
+                        help='Mode (\'ImageToGrayscale\', \'changingImageResolution\', \'SepiaImage\', \'VignetteImage\', \'PixelatingRectImage\')',
+                        type=str,
+                        dest='mode',
+                        default='image')
+    parser.add_argument('-i', '--image',
+                        help='Path to an image',
+                        type=str,
+                        dest='image_path')
+    parser.add_argument('-o', '--output',
+                        help='Output image name',
+                        type=str,
+                        dest='output_image',
+                        default='output.jpg')
+    parser.add_argument('-rw', '--resolutionw',
+                        help='change image resolution width',
+                        type=int,
+                        dest='width')
+    parser.add_argument('-rh', '--resolutionh',
+                        help='change image resolution height',
+                        type=int,
+                        dest='height')
+    parser.add_argument('-rd', '--radius',
+                        help='circle radius for vignette',
+                        type=int,
+                        dest='radius')
+    
+    
+    args = parser.parse_args()
+    return args
+
+def ImageToGrayscale(image_path):
+    if image_path is None:
+        raise ValueError('Empty path to the image')
+        
+        
+    image     = cv.imread(image_path)
+    grayImage = np.zeros(image.shape)
+    
+    grayImage[:, :, 0] = 0.299 * image[:, :, 0] + 0.587 * image[:, :, 1] + 0.114 * image[:, :, 2]
+    grayImage[:, :, 1] = grayImage[:, :, 0]
+    grayImage[:, :, 2] = grayImage[:, :, 0]
+
+    grayImage = grayImage.astype(np.uint8)
+
+
+    cv.imshow('Original image', image)
+    cv.imshow('Gray image', grayImage)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    
+    
+    
+def changingImageResolution(image_path, width, height):
+    if image_path is None:
+        raise ValueError('Empty path to the image')
+
+    image = cv.imread(image_path)
+
+    x = np.linspace(0, image.shape[1] - 1, width)
+    y = np.linspace(0, image.shape[0] - 1, height)
+    x, y = np.meshgrid(x, y)
+
+    x_coords = np.round(x).astype(int)
+    y_coords = np.round(y).astype(int)
+
+    result = image[y_coords, x_coords]
+
+    cv.imshow('Original image', image)
+    cv.imshow('Resolution image', result)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    
+    
+   
+
+def SepiaImage(image_path):
+       
+    if image_path is None:
+        raise ValueError('Empty path to the image')
+
+    image = cv.imread(image_path)
+    sepiaImage = np.zeros(image.shape)
+
+    
+    sepiaImage[:, :, 0] = 0.393 * image[:, :, 2] + 0.769 * image[:, :, 1] + 0.189 * image[:, :, 0]
+    sepiaImage[:, :, 1] = 0.349 * image[:, :, 2] + 0.686 * image[:, :, 1] + 0.168 * image[:, :, 0]
+    sepiaImage[:, :, 2] = 0.272 * image[:, :, 2] + 0.534 * image[:, :, 1] + 0.131 * image[:, :, 0]
+   
+    sepiaImage = np.clip(sepiaImage, 0, 255).astype(np.uint8)
+    
+    sepiaImage = cv.cvtColor(sepiaImage, cv.COLOR_BGR2RGB)
+
+    cv.imshow('Original image', image)
+    cv.imshow('Sepia image', sepiaImage)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    
+    
+def VignetteImage(image_path, radius):
+
+    if image_path is None:
+        raise ValueError('Empty path to the image')
+
+    image = cv.imread(image_path)
+
+    height = image.shape[0]
+    width  = image.shape[1]
+
+    center_x, center_y = width // 2, height // 2
+
+    mask = np.zeros((height, width), dtype=np.float32)
+
+    x, y = np.meshgrid(np.arange(width), np.arange(height))
+
+    distance = np.sqrt((y - center_y) ** 2 + (x - center_x) ** 2)
+    
+    mask = np.ones(distance.shape, dtype=np.float32)
+
+    outside_circle = distance > radius
+    distance_to_border = distance[outside_circle] - radius
+
+    # масштаб затухания
+    strength = 200
+    mask[outside_circle] = np.exp(-(distance_to_border ** 2) / (strength * (10 ** 2)))
+
+    mask = mask / np.max(mask)
+
+    vignette = np.copy(image)
+  
+    vignette[:, :, 0] = vignette[:, :, 0] * mask
+    vignette[:, :, 1] = vignette[:, :, 1] * mask
+    vignette[:, :, 2] = vignette[:, :, 2] * mask
+
+    cv.imshow('Original image', image)
+    cv.imshow('Vignette image', vignette)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+def pixelate_region(image, x1, y1, x2, y2, pixel_size):
+    
+    # pixel_image = image.copy()
+
+    x1, y1 = max(0, x1), max(0, y1)
+    x2, y2 = min(image.shape[1], x2), min(image.shape[0], y2)
+    
+    # Копируем выбранную область
+    roi = image[y1:y2, x1:x2]
+    roi_h, roi_w = roi.shape[:2]
+
+    # Размер уменьшенной копии
+    small_h = roi_h // pixel_size
+    small_w = roi_w // pixel_size
+
+    for i in range(0, small_h):
+        for j in range(0, small_w):
+            # Определяем область пикселя в оригинальном изображении
+            start_y = i * pixel_size
+            end_y = start_y + pixel_size
+            start_x = j * pixel_size
+            end_x = start_x + pixel_size
+            
+            # Обрабатываем, чтобы не выходить за границы
+            end_y = min(end_y, roi_h)
+            end_x = min(end_x, roi_w)
+            
+            # Берем среднее значение цвета в области пикселя
+            pixel_block = roi[start_y:end_y, start_x:end_x]
+            avg_color = pixel_block.mean(axis=(0, 1)).astype(int)
+
+            # Заполняем этот блок средним цветом
+            roi[start_y:end_y, start_x:end_x] = avg_color
+
+    # Заменяем пикселизированную область на исходном изображении
+    image[y1:y2, x1:x2] = roi
+
+
+    
+    
+def draw_rectangle(event, x, y, flags, param):
+    global rect, drawing, img, pixel_image, pixelate_size
+
+    if event == cv.EVENT_LBUTTONDOWN:
+        drawing = True
+        rect = (x, y, x, y)
+    
+    elif event == cv.EVENT_MOUSEMOVE:
+        if drawing:
+            img[:] = pixel_image.copy()  
+            cv.rectangle(img, (rect[0], rect[1]), (x, y), (0, 255, 0), 2)
+            rect = (rect[0], rect[1], x, y)
+    
+    elif event == cv.EVENT_LBUTTONUP:
+        drawing = False
+        pixelate_region(img, rect[0], rect[1], x, y, pixelate_size)
+        pixel_image[:] = img.copy()  # Обновляем копию
+        
+def PixelatingRectImage(image_path):
+    global img, pixel_image, rect, drawing, pixelate_size
+
+    pixelate_size = 10
+    rect = (0, 0, 1, 1)
+    drawing = False
+    
+    if image_path is None:
+        raise ValueError('Empty path to the image')
+
+    
+    img = cv.imread(image_path)
+    pixel_image = img.copy()
+
+    
+    cv.namedWindow('Image')
+    cv.setMouseCallback('Image', draw_rectangle)
+
+    while True:
+        cv.imshow('Image', img)
+        if cv.waitKey(1) & 0xFF == 27:  # Нажмите ESC для выхода
+            break
+
+    cv.destroyAllWindows()
+    
+    
+    
+def main():
+    args = cli_argument_parser()
+    
+    if args.mode == 'ImageToGrayscale':
+        ImageToGrayscale(args.image_path)
+    elif args.mode == 'changingImageResolution':
+        changingImageResolution(args.image_path, args.width, args.height)
+    elif args.mode == 'SepiaImage':
+        SepiaImage(args.image_path)
+    elif args.mode == 'VignetteImage':
+        VignetteImage(args.image_path, args.radius)
+    elif args.mode == 'PixelatingRectImage':
+        PixelatingRectImage(args.image_path)
+    else:
+        raise 'Unsupported \'mode\' value'
+
+
+if __name__ == '__main__':
+    sys.exit(main() or 0)
