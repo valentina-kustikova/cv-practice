8022305f55341da8ea90ec891d311f8b7eb3aaa0
NastyaMortina <90023831+NastyaMortina@users.noreply.github.com>
2024-10-27 22:57:14 +0300

    Update CV_lab1.py

--- Mortina_Lab_1/CV_lab1.py
+++ Mortina_Lab_1/CV_lab1.py
@@ -19,7 +19,7 @@ def cli_argument_parser():
                         dest='output_image',
                         default='output.jpg')
     parser.add_argument('-m', '--mode',
-                        help='Mode (\'image\', \'image_to_grayscale\', \'image_resize\', \'sepia\', \'vignette\', \'pixelation\')',
+                        help='Mode (\'image\', \'gray_image\', \'resize_image\', \'sepia_image\', \'vignette_image\', \'pixel_mage\')',
                         type=str,
                         dest='mode',
                         default='image')
@@ -28,221 +28,193 @@ def cli_argument_parser():
                         type=float,
                         dest='value',
                         default='1')
-    parser.add_argument('-p', '--pixel_size',
-                        help='size pf pixel',                   
-                        type=str,
-                        dest='pixel_size',
-                        default='5')
+    parser.add_argument('-r', '--radius',
+                        help='radius',                   
+                        type=int,
+                        dest='radius',
+                        default='150')
+    parser.add_argument('-p', '--pixelate_size',
+                        help='pixelate size',
+                        type=int,
+                        dest='pixelate_size',
+                        default='15')    
     
     args = parser.parse_args()
     return args
 
-#Пишем функцию, которая занимается базовыми операциями с изображениями с помощью библиотеки OpenCV
-def highgui_image_samples(image_path, output_image):
-    if image_path is None:
-        raise ValueError('Empty path to the image')
-
-    #Загрузка изображения
-    image = cv.imread(image_path)
-    height, width, nchannels = image.shape
-
-    #Отображение изображения
-    cv.imshow('Init image', image)
-    cv.waitKey(0)
-    
-    #Сохранение изображения
-    cv.imwrite(output_image, image)
-
-    #Освобождение ресурсов для последующей работы    
-    cv.destroyAllWindows()
-
 
 #Функция перевода изображения в оттенки серого
-def image_to_grayscale(image_path, output_image):
-    if image_path is None:
-        raise ValueError('Empty path to the image')
-        
-    #Загрузка изображения
-    image = cv.imread(image_path)
+def image_to_grayscale(image):
     height, width, nchannels = image.shape
     
     grey_image = np.zeros((height, width, 3), dtype=np.uint8)
-
-    for i in range(height):
-        for j in range(width):
-            R, G, B = image[i, j]
-            grey_value = int(0.2989 * R + 0.5870 * G + 0.1140 * B) 
             
-            grey_image[i, j] = grey_value
+    R = image[:,:,0] 
+    G = image[:,:,1] 
+    B = image[:,:,2] 
+            
+    grey_image = 0.2989 * R + 0.5870 * G + 0.1140 * B
     
-    #Отображение изображения
-    cv.imshow('Init image', image)
-    cv.imshow('Output image', grey_image)
-    cv.waitKey(0)
+    grey_image = image[:,:,0] 
+    grey_image = image[:,:,1] 
+    grey_image = image[:,:,2] 
     
-    #Сохранение изображения
-    cv.imwrite(output_image, grey_image)
-
-    #Освобождение ресурсов для последующей работы    
-    cv.destroyAllWindows()
-
+    return grey_image
+    
 
 #Функция изменения разрешения изображения
-def image_resize(image_path, output_image, value):
-    if image_path is None:
-        raise ValueError('Empty path to the image')
-        
-    #Загрузка изображения
-    image = cv.imread(image_path)
+def image_resize(image, value):
     height, width, nchannels = image.shape
-    
     
     new_height = int(height*value)
     new_width = int(width*value)
     
     resize_image = np.zeros((new_height, new_width, 3), dtype=np.uint8)
-    
-    for i in range(new_height):
-        for j in range(new_width):
-            new_x = int(j/value)
-            new_y = int(i/value)
+             
+    new_y = np.floor(np.arange(new_height) / value).astype(int)        
+    new_x = np.floor(np.arange(new_width) / value).astype(int)
 
-            resize_image[i, j] = image[new_y, new_x]
+    resize_image = image[new_y[:, None], new_x]
 
-    #Отображение изображения
-    cv.imshow('Init image', image)
-    cv.imshow('Output image', resize_image)
-    cv.waitKey(0)
-
-    #Сохранение изображения
-    cv.imwrite(output_image, resize_image)
-
-    #Освобождение ресурсов для последующей работы    
-    cv.destroyAllWindows()
+    return resize_image
 
 
 #Функция применения фотоэффекта сепии к изображению.
-def sepia(image_path, output_image):
-    if image_path is None:
-        raise ValueError('Empty path to the image')
-        
-    #Загрузка изображения
-    image = cv.imread(image_path)
+def sepia(image):
     height, width, nchannels = image.shape
 
     sepia_image = np.zeros((height, width, 3), dtype=np.uint8)
+
+    B = image[:,:,0] 
+    G = image[:,:,1] 
+    R = image[:,:,2] 
     
-    for i in range(height):
-        for j in range(width):
-            B, G, R = image[i, j]
-            sepiaR = min(int(0.393 * R + 0.769 * G + 0.189 * B), 255)
-            sepiaG = min(int(0.349 * R + 0.686 * G + 0.168 * B), 255)
-            sepiaB = min(int(0.272 * R + 0.534 * G + 0.131 * B), 255)
-
-            sepia_image[i, j] = [sepiaB, sepiaG, sepiaR]
-
-    #Отображение изображения
-    cv.imshow('Init image', image)
-    cv.imshow('Output image', sepia_image)
-    cv.waitKey(0)
-
-    #Сохранение изображения
-    cv.imwrite(output_image, sepia_image)
-
-    #Освобождение ресурсов для последующей работы    
-    cv.destroyAllWindows()
-     
+    sepia_image[:,:,0] = np.clip(0.272 * R + 0.534 * G + 0.131 * B, 0, 255) 
+    sepia_image[:,:,1] = np.clip(0.349 * R + 0.686 * G + 0.168 * B, 0, 255)
+    sepia_image[:,:,2] = np.clip(0.393 * R + 0.769 * G + 0.189 * B, 0, 255)
+    
+    return sepia_image
  
+    
 #Функция применения фотоэффекта виньетки к изображению.
-def vignette(image_path, output_image):
-    if image_path is None:
-        raise ValueError('Empty path to the image')
-        
-    #Загрузка изображения
-    image = cv.imread(image_path)
+def vignette(image, radius):
     height, width, nchannels = image.shape
 
     vignette_image = np.zeros((height, width, 3), dtype=np.uint8)
     
-    x = int(width / 2)
-    y = int(height / 2)
-    
-    for i in range(height):
-        for j in range(width):
-            x_value = 1 - (abs(j - x) / x)
-            y_value = 1 - (abs(i - y) / y)
-            vignette_image[i, j] = image[i, j]  * x_value * y_value
-    
-    #Отображение изображения
-    cv.imshow('Init image', image)
-    cv.imshow('Output image', vignette_image)
-    cv.waitKey(0)
+    height = image.shape[0]
+    width = image.shape[1]
 
-    #Сохранение изображения
-    cv.imwrite(output_image, vignette_image)
+    x = width // 2
+    y = height // 2
+    y_value, x_value = np.indices((height, width))
+    
+    distance = np.sqrt((x_value - x) ** 2 + (y_value - y) ** 2)
+    coef_vin = np.clip(1 - distance / radius, 0, 1)
+    vignette_image = (image * coef_vin[:, :, np.newaxis]).astype(np.uint8)
+           
+    return vignette_image
+    
 
-    #Освобождение ресурсов для последующей работы    
-    cv.destroyAllWindows()
-    
-    
 #Функция пикселизации заданной прямоугольной области изображения
-def pixelation(image_path, output_image, pixel_size):
-    if image_path is None:
-        raise ValueError('Empty path to the image')
+
+#Функция для пексилизации области изображения
+def image_pixel(image, x1, y1, x2, y2, pixel_size):
+    x1, y1 = max(0, x1), max(0, y1)
+    x2, y2 = min(image.shape[1], x2), min(image.shape[0], y2)
+    
+    reg = image[y1:y2, x1:x2]
+    reg_h, reg_w = reg.shape[:2]
+
+    new_h = reg_h // pixel_size
+    new_w = reg_w // pixel_size
+
+    for i in range(0, new_h):
+        for j in range(0, new_w):
+            
+            start_y = i * pixel_size
+            end_y = start_y + pixel_size
+            start_x = j * pixel_size
+            end_x = start_x + pixel_size
+            end_y = min(end_y, reg_h)
+            end_x = min(end_x, reg_w)
+            
+            pixel_block = reg[start_y:end_y, start_x:end_x]
+            avg_color = pixel_block.mean(axis=(0, 1)).astype(int)
+            reg[start_y:end_y, start_x:end_x] = avg_color
+
+    image[y1:y2, x1:x2] = reg
+
+#Функция, отвечающая за обработку события мыши для рисования прямоугольника и пикселизации выбранной области
+def draw(event, x, y, flags, param):
+    global rect, drawing, img, pixel_image, pixelate_size
+
+    if event == cv.EVENT_LBUTTONDOWN:
+        drawing = True
+        rect = (x, y, x, y)
+    
+    elif event == cv.EVENT_MOUSEMOVE:
+        if drawing:
+            img[:] = pixel_image.copy()  
+            cv.rectangle(img, (rect[0], rect[1]), (x, y), (0, 255, 0), 2)
+            rect = (rect[0], rect[1], x, y)
+    
+    elif event == cv.EVENT_LBUTTONUP:
+        drawing = False
+        img[:] = pixel_image.copy()  
+        image_pixel(img, rect[0], rect[1], x, y, pixelate_size)
+        pixel_image[:] = img.copy() 
         
-    #Загрузка изображения
-    image = cv.imread(image_path)
-    height, width, nchannels = image.shape
+#Сама функция пикселизации        
+def pixel_image(image, size):
+    global img, pixel_image, rect, drawing, pixelate_size
 
-    pixelation_image = np.zeros((height, width, 3), dtype=np.uint8)
+    pixelate_size = size
+    rect = (0, 0, 1, 1)
+    drawing = False
     
-    pixelation_image = np.copy(image)
+    pixel_image = image.copy()
+    img = image.copy()
     
-    x1 = width // 4
-    x2 = int(width * 0.5)
-    y1 = height // 4
-    y2 = int(height * 0.5)
+    cv.namedWindow('Image')
+    cv.setMouseCallback('Image', draw)
 
-    for i in range(x1, x2, pixel_size):
-        for j in range(y1, y2, pixel_size):
-            
-            region = image[j:j + pixel_size, i:i + pixel_size]
-            
-            color = [np.mean(region[:, :, 0]), np.mean(region[:, :, 1]), np.mean(region[:, :, 2])]  
-            
-            pixelation_image[j:j + pixel_size, i:i + pixel_size] = color
-            
-    #Отображение изображения
-    cv.imshow('Init image', image)
-    cv.imshow('Output image', pixelation_image)
-    cv.waitKey(0)
+    while True:
+        cv.imshow('Image', img)
+        if cv.waitKey(1) & 0xFF ==ord('0'):
+            break
 
-    #Сохранение изображения
-    cv.imwrite(output_image, pixelation_image)
-
-    #Освобождение ресурсов для последующей работы    
-    cv.destroyAllWindows()
     
-     
-#Пишем функцию, которая запускает необходимый сценарий, исходя из аргументов командной строки
+#Пишем функцию, которая запускает необходимый сценарий, исходя из аргументов командной строки, а также вызов чтения и вывода изображений
 def main():
     args = cli_argument_parser()
     
-    if args.mode == 'image':
-        highgui_image_samples(args.image_path, args.output_image)
-    elif args.mode == 'image_to_grayscale':
-        image_to_grayscale(args.image_path, args.output_image)
-    elif args.mode == 'image_resize':
-        image_resize(args.image_path, args.output_image, args.value)
-    elif args.mode == 'sepia':
-        sepia(args.image_path, args.output_image)
-    elif args.mode == 'vignette':
-        vignette(args.image_path, args.output_image)
-    elif args.mode == 'pixelation':
-        pixelation(args.image_path, args.output_image, int(args.pixel_size))
+    image_path = args.image_path
+    if image_path is None:
+       raise ValueError('Empty path to the image')
+    image = cv.imread(image_path)
+   
+    
+    #if args.mode == 'image':
+        #highgui_image_samples(args.image_path, args.output_image)
+    if args.mode == 'gray_image':
+        filtr_image = image_to_grayscale(image)
+    elif args.mode == 'resize_image':
+        filtr_image = image_resize(image, args.value)
+    elif args.mode == 'sepia_image':
+        filtr_image = sepia(image)
+    elif args.mode == 'vignette_image':
+        filtr_image = vignette(image, args.radius)
+    elif args.mode == 'pixel_image':
+        filtr_image = pixel_image(image, args.pixelate_size)
     else:
         raise 'Unsupported \'mode\' value'
+        
+    cv.imshow('Init image', image)
+    cv.imshow('Output image', filtr_image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 if __name__ == '__main__':
     sys.exit(main() or 0)
-
+    
