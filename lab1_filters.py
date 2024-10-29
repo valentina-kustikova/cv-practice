b0ddc0a487a1b12ff44186bb7a84b9eccc5b7b3c
CoolEgor61 <vip.Mazda003@bk.ru>
2024-10-30 00:52:25 +0300

    Lab 1 0.0.2a

--- lab1_filters.py
+++ lab1_filters.py
@@ -8,8 +8,9 @@ def toShapesOfGray(dst):
     # Вывод функции - исходное изображение в оттенках серого
     
     b,g,r=cv.split(dst)
-    gray = np.uint8(0.299*r) + np.uint8(0.587*g) + np.uint8(0.114*b)
-    return gray
+    gray = 0.299*r + 0.587*g + 0.114*b
+    gray[gray>255]=255
+    return gray.astype(np.uint8)
 
 def changeSize(dst, desired_height, desired_width):
     
@@ -31,27 +32,30 @@ def toSepia(dst):
     # Вывод функции - сепия исходного изображения
     
     b1,g1,r1=cv.split(dst)
-    b=np.uint8(0.272*r1) + np.uint8(0.534*g1) + np.uint8(0.131*b1)
-    g=np.uint8(0.349*r1) + np.uint8(0.686*g1) + np.uint8(0.168*b1)
-    r=np.uint8(0.393*r1) + np.uint8(0.769*g1) + np.uint8(0.189*b1)
+    b=0.272*r1 + 0.534*g1 + 0.131*b1
+    g=0.349*r1 + 0.686*g1 + 0.168*b1
+    r=0.393*r1 + 0.769*g1 + 0.189*b1
     sepia=cv.merge((b,g,r))
-    return sepia
+    sepia[sepia>255]=255
+    return sepia.astype(np.uint8)
 
-def vignette(dst,r): # Работает сомнительно
+def vignette(dst,r): # Возведение в степень 1.5 вышло методом научного тыка...
 
     # dst - исходное изображение
     # r - радиус незатемненной области
     # Вывод функции - фильтр виньетки на исходном изображении
 
     height, width, channels = dst.shape
-    vign = dst
-    mask = np.empty((height,width,3), np.uint8)
-    for i in range (0,height//2):
-        mask[i] = mask[height-i-1] = i
-    gray =toShapesOfGray(dst)
-    mask = cv.circle(mask, (width//2,height//2), r, (255,255,255),thickness=-1)
-    mask = cv.bitwise_and(mask, dst)
-    return mask
+    rq=r*r
+    vign = np.zeros((height,width,3))
+    X,Y=np.meshgrid(np.arange(width),np.arange(height))
+    distances=(Y-height//2)*(Y-height//2)+(X-width//2)*(X-width//2)
+    norm_distances=distances/np.max(distances)
+    norm_rq=rq/np.max(distances)
+    vign[:,:,0]=dst[:,:,0]*(1-(norm_distances/norm_rq))**(1.5)
+    vign[:,:,1]=dst[:,:,1]*(1-(norm_distances/norm_rq))**(1.5)
+    vign[:,:,2]=dst[:,:,2]*(1-(norm_distances/norm_rq))**(1.5)
+    return vign.astype(np.uint8)
 
 def pixelization(dst,pix_size,x1,y1,x2,y2): 
     
@@ -68,10 +72,8 @@ def pixelization(dst,pix_size,x1,y1,x2,y2):
     for i in range(0,ky):
         tmp[i]=np.hsplit(tmp[i], kx)
     for i in range(0,ky):
-        for j in range(0,kx):
-            tmp[i][j]=tmp[random.randint(0,ky-1)][random.randint(0,kx-1)]
+        random.shuffle(tmp[i])
     for i in range(0,ky):
         tmp[i]=np.hstack(tmp[i])
-    tmp=np.vstack(tmp)
-    dst[y1:(y1+pix_size*ky),x1:(x1+pix_size*kx)]=tmp
+    dst[y1:(y1+pix_size*ky),x1:(x1+pix_size*kx)]=np.vstack(tmp)
     return dst
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import cv2 as cv
 import lab1_filters as lf
 
-image = input()
-if (image != None):
-    img = cv.imread(image)
-    cv.imshow("Image", img)
-    cv.imshow("Gray Image", lf.toShapesOfGray(img))
-    cv.imshow("200x200 Image", lf.changeSize(img, 200, 200))
-    cv.imshow("Sepia Image", lf.toSepia(img))
-    cv.imshow("Vignette Image", lf.vignette(img, 200))
-    cv.imshow("Pixelizied Image", lf.pixelization(img, 12, 100, 100, 400, 400))
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+img = cv.imread("12.jpeg")
+cv.imshow("Image", img)
+cv.imshow("Gray Image", lf.toShapesOfGray(img))
+cv.imshow("200x200 Image", lf.changeSize(img, 200, 200))
+cv.imshow("Sepia Image", lf.toSepia(img))
+cv.imshow("Vignette Image", lf.vignette(img, 450))
+cv.imshow("Pixelizied Image", lf.pixelization(img, 12, 0, 0, 400, 400))
+cv.waitKey(0)
+cv.destroyAllWindows()
