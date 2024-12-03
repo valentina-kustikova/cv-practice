bca79a49a52e6550b176898564d7ecc2bbdd6410
BlackAngel2108 <luba.sobol.nn@mail.ru>
2024-12-04 00:04:48 +0300

    fixed

--- Sobol_Luba/Sobol_Luba_lab1/lab1.py
+++ Sobol_Luba/Sobol_Luba_lab1/lab1.py
@@ -35,11 +35,26 @@ def cli_argument_parser():
                         type=str,
                         default='5',
                         dest='pixel_size',)
-
+    parser.add_argument('-r', '--radius',
+                        help='radius vignette',                   
+                        type=str,
+                        default='10',
+                        dest='radius',)
     args = parser.parse_args()
 
     return args
 
+
+def load_image(image_path):
+    if image_path is None:
+        raise ValueError('Empty path to the image')
+
+    # Загрузка изображения
+    image = cv.imread(image_path)
+    if image is None:
+        raise ValueError("Изображение не загружено. Проверьте путь к файлу.")
+
+    return image
 
 def highgui_samples(in_filename, out_filename):
     img = cv.imread(in_filename)
@@ -50,24 +65,7 @@ def highgui_samples(in_filename, out_filename):
     
     cv.destroyAllWindows()
 
-def grey_color(image_path):
-    if image_path is None:
-        raise ValueError('Empty path to the image')
-    # Загрузка изображения
-    image = cv.imread(image_path)
-    #height, width, _ = image.shape
-    #grey_image = np.zeros((height, width, 3), dtype=np.uint8)
-
-    # for i in range(height):
-    #     for j in range(width):
-    #         pixel = image[i, j]
-
-    #         blue_channel = pixel[0]  # Синий канал
-    #         green_channel = pixel[1]  # Зелёный канал
-    #         red_channel = pixel[2]  # Красный канал
-
-    #         grey_value = int(blue_channel * 0.114 + green_channel * 0.587 + red_channel * 0.299)
-    #         grey_image[i, j] = [grey_value, grey_value, grey_value]
+def grey_color(image):
     blue_channel = image[:, :, 0] 
     green_channel = image[:, :, 1] 
     red_channel = image[:, :, 2] 
@@ -75,16 +73,10 @@ def grey_color(image_path):
     grey_values = (blue_channel * 0.114 + green_channel * 0.587 + red_channel * 0.299).astype(np.uint8) 
     
     grey_image = np.stack((grey_values, grey_values, grey_values), axis=-1)
-    col=np.vstack([image, grey_image])
-    cv.imshow('Gray image',col)
-    cv.waitKey()
+
     return grey_image
 
-def resize_image(image_path, new_width, new_height):
-    if image_path is None:
-        raise ValueError('Empty path to the image')
-    # Загрузка изображения
-    image = cv.imread(image_path)
+def resize_image(image, new_width, new_height):
     height, width, _ = image.shape
     resized_image = np.zeros((new_height, new_width, 3), dtype=np.uint8)
 
@@ -94,32 +86,10 @@ def resize_image(image_path, new_width, new_height):
             orig_y = int(i * height / new_height)
 
             resized_image[i, j] = image[orig_y, orig_x]
-    cv.imshow('resized_image',resized_image)
-    cv.waitKey()
+
     return resized_image
 
-def sepia_filter(image_path):
-    if image_path is None:
-        raise ValueError('Empty path to the image')
-    # Загрузка изображения
-    image = cv.imread(image_path)
-    # height, width, _ = image.shape
-    # sepia_image = np.zeros((height, width, 3), dtype=np.uint8)
-
-    # for i in range(height):
-    #     for j in range(width):
-    #         pixel = image[i, j]
-
-    #         blue_channel = pixel[0]  # Синий канал
-    #         green_channel = pixel[1]  # Зелёный канал
-    #         red_channel = pixel[2]  # Красный канал
-
-    #         tr = int(0.393 * red_channel + 0.769 * green_channel + 0.189 * blue_channel)
-    #         tg = int(0.349 * red_channel + 0.686 * green_channel + 0.168 * blue_channel)
-    #         tb = int(0.272 * red_channel + 0.534 * green_channel + 0.131 * blue_channel)
-
-    #         # Ограничиваем значения до 255
-    #         sepia_image[i, j] = [min(tb, 255), min(tg, 255), min(tr, 255)]
+def sepia_filter(image):
     blue_channel = image[:, :, 0] 
     green_channel = image[:, :, 1] 
     red_channel = image[:, :, 2] 
@@ -129,100 +99,127 @@ def sepia_filter(image_path):
     sepia_red = np.clip(0.393 * red_channel + 0.769 * green_channel + 0.189 * blue_channel, 0, 255).astype(np.uint8) 
      
     sepia_image = np.stack((sepia_blue, sepia_green, sepia_red), axis=-1)
-    col=np.vstack([image, sepia_image])
-    cv.imshow('sepia_image',col)
-    cv.waitKey()
+
     return sepia_image
 
-def vignette_filter(image_path):
-    if image_path is None:
-        raise ValueError('Empty path to the image')
-    # Загрузка изображения
-    image = cv.imread(image_path)
+
+def vignette_filter(image, radius):
     height, width, channels = image.shape
-    # center_x, center_y = width // 2, height // 2
-    # max_dist = np.sqrt(center_x**2 + center_y**2)
 
-    # vignette_image = np.zeros((height, width, 3), dtype=np.uint8)
+    center_x = width // 2
+    center_y = height // 2
 
-    # for i in range(height):
-    #     for j in range(width):
-    #         pixel = image[i, j]
+    x = np.arange(width)
+    y = np.arange(height)
+    xx, yy = np.meshgrid(x, y)
+    distances = np.sqrt((xx - center_x)**2 + (yy - center_y)**2)
 
-    #         # Расстояние от центра
-    #         dx = (j - center_x)**2
-    #         dy = (i - center_y)**2
-    #         distance = np.sqrt(dx + dy)
+    # Вычислите фактор виньетирования
+    factor = np.ones_like(distances, dtype=np.float32)
+    factor[distances > radius] = (radius / distances[distances > radius])
+    factor = np.clip(factor, 0.1, 1.0)  # Не даем фактору стать слишком маленьким
 
-    #         # Виньетирование, чем дальше от центра, тем темнее пиксель
-    #         factor = (max_dist - distance) / max_dist
-    #         factor = max(factor, 0.1)  # Не даем фактору стать слишком маленьким
-
-    #         vignette_image[i, j] = [int(channel * factor) for channel in pixel]
-    center_x = width // 2 
-    center_y = height // 2 
- 
-    max_dist = np.sqrt(center_x**2 + center_y**2) 
-
-    x = np.arange(width) 
-    y = np.arange(height) 
-    xx, yy = np.meshgrid(x, y) 
-    distances = np.sqrt((xx - center_x)**2 + (yy - center_y)**2) 
-    
-    factor = (max_dist - distances) / max_dist 
-    factor = np.clip(factor, 0.1, 1.0)  # Не даем фактору стать слишком маленьким 
-    
-    vignette_image = np.zeros_like(image) 
-    for channel in range(channels): 
+    # Примените фактор виньетирования к каждому каналу
+    vignette_image = np.zeros_like(image)
+    for channel in range(channels):
         vignette_image[:, :, channel] = np.clip(image[:, :, channel] * factor, 0, 255).astype(np.uint8)
-    col=np.vstack([image, vignette_image])
-    cv.imshow('vignette_image',col)
-    cv.waitKey()
+
     return vignette_image
 
-def pixelate_area(image_path, pixel_size):
-    if image_path is None:
-        raise ValueError('Empty path to the image')
-    # Загрузка изображения
-    image = cv.imread(image_path)
-    height, width, _ = image.shape
+
+x1, y1, x2, y2 = -1, -1, -1, -1
+drawing = False
+
+def select_area(event, x, y, flags, image):
+    global x1, y1, x2, y2, drawing
+
+    if event == cv.EVENT_LBUTTONDOWN:
+        drawing = True
+        x1, y1 = x, y
+
+    elif event == cv.EVENT_MOUSEMOVE:
+        if drawing:
+            x2, y2 = x, y
+
+    elif event == cv.EVENT_LBUTTONUP:
+        drawing = False
+        x2, y2 = x, y
+
+        # Ограничиваем координаты выбранной области, чтобы они не выходили за пределы изображения
+        height, width = image.shape[:2]
+        x1 = max(0, min(x1, width))
+        y1 = max(0, min(y1, height))
+        x2 = max(0, min(x2, width))
+        y2 = max(0, min(y2, height))
+
+def pixelate_area(image, pixel_size):
     pixelated_image = np.copy(image)
 
-    x1, y1 = (width // 4 ,height // 4) 
-    x2, y2 = (3 * width // 4, 3 * height // 4)
+    # Создаем окно для отображения изображения
+    cv.namedWindow('image')
+    cv.setMouseCallback('image', select_area, image)
 
-    for i in range(y1, y2, pixel_size):
-        for j in range(x1, x2, pixel_size):
-            # Определяем усреднённый цвет для блока
-            block = image[i:i+pixel_size, j:j+pixel_size]
-            avg_color = block.mean(axis=(0, 1)).astype(int)
+    while True:
+        temp_image = np.copy(image)
+        if x1 != -1 and y1 != -1 and x2 != -1 and y2 != -1:
+            cv.rectangle(temp_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-            # Закрашиваем блок усреднённым цветом
-            pixelated_image[i:i+pixel_size, j:j+pixel_size] = avg_color
-    col=np.vstack([image, pixelated_image])
-    cv.imshow('vignette_image',col)
-    cv.waitKey()
-    return pixelated_image
+        cv.imshow('image', temp_image)
+        key = cv.waitKey(1) & 0xFF
+        
+        if key == 13:  # Нажмите 'Enter' для пикселизации выбранной области
+            if x1 != -1 and y1 != -1 and x2 != -1 and y2 != -1:
+                for i in range(y1, y2, pixel_size):
+                    for j in range(x1, x2, pixel_size):
+                        # Определяем усреднённый цвет для блока
+                        block = image[i:i+pixel_size, j:j+pixel_size]
+                        avg_color = block.mean(axis=(0, 1)).astype(int)
+
+                        # Закрашиваем блок усреднённым цветом
+                        pixelated_image[i:i+pixel_size, j:j+pixel_size] = avg_color
+
+                # Объединяем изначальное изображение и пикселизированную область
+                combined_image = np.copy(image)
+                combined_image[y1:y2, x1:x2] = pixelated_image[y1:y2, x1:x2]
+
+                cv.destroyAllWindows()
+                break
+        
+        elif key == 27:  # Нажмите 'Esc' для выхода
+            break
+
+    return combined_image
+
+def showImg(img, new_img, named):
+    col=np.vstack([img, new_img])
+    cv.imshow(named,col)
+    cv.waitKey(0)
 
 def main():
     args = cli_argument_parser()
-    
+    image = load_image(args.image_path)
+
     if args.mode == 'image':
         highgui_samples(args.image_path, args.out_image_path)
     elif args.mode == 'grey_color':
-        grey_color(args.image_path)
+        grey_image = grey_color(image)
+        showImg(image, grey_image, 'grey_img')
     elif args.mode == 'resize_image':
-        resize_image(args.image_path,int(args.width), int(args.height))
+        resized_image = resize_image(image, int(args.width), int(args.height))
+        cv.imshow('resize_image', resized_image)
+        cv.waitKey(0)
     elif args.mode == 'sepia_filter':
-        sepia_filter(args.image_path)
+        sepia_image = sepia_filter(image)
+        showImg(image, sepia_image, 'sepia_img')
     elif args.mode == 'vignette_filter':
-        vignette_filter(args.image_path)
+        vignette_image = vignette_filter(image, int(args.radius))
+        showImg(image, vignette_image, 'vignette_image')
     elif args.mode == 'pixelated':
-        pixelate_area(args.image_path,int(args.pixel_size))
+        pixelate_image = pixelate_area(image, int(args.pixel_size))
+        showImg(image, pixelate_image, 'pixelate_image')
     else:
         raise ValueError('Unsupported mode')
 
 
 if __name__ == '__main__':
     sys.exit(main() or 0)
-
