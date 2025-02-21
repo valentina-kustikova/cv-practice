743d5501a7fe3187c7c0c7641b86b7677e416704
Ach1L <roman.kanakov.3007@gmail.com>
2025-02-21 18:27:38 +0300

    Third lab, README fix

--- KanakovRA/lab3/lab3.py
+++ KanakovRA/lab3/lab3.py
@@ -1,20 +1,21 @@
 import cv2
 import numpy as np
 import argparse
-from sklearn.cluster import KMeans
-import os
 from subprocess import run
-from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay, classification_report
-import matplotlib.pyplot as plt
+import os
+import re
+from sklearn.cluster import KMeans
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
 from sklearn.svm import SVC
+import matplotlib.pyplot as plt
 
 
 # Организация работы с аргументами командной строки
-def parse_args():
+def parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-tr', '--trainPath', type = str)
-    parser.add_argument('-te', '--testPath', type = str)
-    parser.add_argument('-c', '--clusters', type = int, default = 1000)
+    parser.add_argument('-tr', '--trainPath', type = str, default = "cvd/train")
+    parser.add_argument('-te', '--testPath', type = str, default = "cvd/test")
+    parser.add_argument('-c', '--clusters', type = int, default = 41)
     return parser.parse_args()
 
 
@@ -29,16 +30,41 @@ def loadImages(folder: str) -> list:
     return images
 
 
-# Загрузка меток
-def loadLabels(folder: str) -> list:
-    # Выполнение bash-скрипта для генерации файла с метками
-    run(['bash', f'{folder}/labels.sh'])
-    with open(f'{folder}/labels.txt', 'r', encoding = 'utf-8') as file:
-        return list(''.join(char for char in file.read() if char != '\n'))
+def loadTrainLabels(folder: str) -> np.array:
+    labels = []
+
+    for filename in os.listdir(folder):
+        # Получаем имя файла без расширения
+        name, _ = os.path.splitext(filename)
+
+        # Проверяем, начинается ли имя файла с 'cat' или 'dog'
+        if name.startswith('cat'):
+            labels.append(0)
+        elif name.startswith('dog'):
+            labels.append(1)
+    return np.array(labels)
+
+
+def loadTestLabels(folder: str) -> np.array:
+    labels = []
+ 
+    for filename in os.listdir(folder):
+        # Получаем имя файла без расширения
+        name, _ = os.path.splitext(filename)
+
+        # Проверяем, является ли имя файла числом
+        if re.match(r'^\d+$', name):
+            number = int(name)
+            # Если число меньше 101, добавляем 0 в labels
+            if number < 101:
+                labels.append(0)
+            else:
+                labels.append(1)
+    return np.array(labels)
 
 
 # Извлечение SIFT-дескрипторов из изображений
-def getDescriptors(images):
+def getDescriptors(images: list) -> list:
     sift = cv2.SIFT_create()
     allDescriptors = []
     for image in images:
@@ -50,14 +76,14 @@ def getDescriptors(images):
 
 
 # Кластеризация по дескрипторам (создание мешка слов)
-def createBow(descriptors, clusters):
+def createBow(descriptors: list, clusters: int):
     kmeans = KMeans(n_clusters = clusters, random_state = 42)
     kmeans = kmeans.fit(np.vstack(descriptors))
     return kmeans
 
 
-# Извлечение признаков на основе мешка слов для тестовых данных
-def getFeatures(descriptors, kmeans, clusters):
+# Извлечение признаков на основе мешка слов
+def getFeatures(descriptors: list, kmeans, clusters: int):
     features = []
     for desc in descriptors:
         histogram = np.zeros(clusters)
@@ -69,32 +95,32 @@ def getFeatures(descriptors, kmeans, clusters):
 
 
 def main():
-    args = parse_args()
+    args = parse()
 
-    trainLabels, trainImages = loadLabels(args.trainPath), loadImages(args.trainPath)
-    testLabels, testImages = loadLabels(args.testPath), loadImages(args.testPath)
-
+    trainLabels, trainImages = loadTrainLabels(args.trainPath), loadImages(args.trainPath)
+    testLabels, testImages = loadTestLabels(args.testPath), loadImages(args.testPath)
+    
     trainDesc = getDescriptors(trainImages)
     testDesc = getDescriptors(testImages)
 
-    kmeans = createBow(trainDesc, args.clusters)
+    c = args.clusters
+    
+    kmeans = createBow(trainDesc, c)
 
-    trainFeatures = getFeatures(trainDesc, kmeans, args.clusters)
-    testFeatures = getFeatures(testDesc, kmeans, args.clusters)
+    trainFeatures = getFeatures(trainDesc, kmeans, c)
+    testFeatures = getFeatures(testDesc, kmeans, c)
 
-    # Используем метод опорных векторов для разделения на 2 класса
-    model = SVC(kernel = 'linear', random_state = 42)
+    # Используем метод опорных векторов с линейным ядром для разделения на 2 класса
+    model = SVC(kernel = 'rbf', random_state = 42)
     model.fit(trainFeatures, trainLabels)
     pred = model.predict(testFeatures)
 
-    accuracy = accuracy_score(testLabels, pred)
-    print(f"Accuracy is = {accuracy:.2f}")
-
+    # Вывод в консоль основной статистики
     print(classification_report(testLabels, pred, target_names = ["Cat", "Dog"]))
-
+    
     # Визуализация матрицы рассогласований
     cm = confusion_matrix(testLabels, pred)
-    ConfusionMatrixDisplay(cm).plot(cmap = 'Reds')
+    ConfusionMatrixDisplay(cm, display_labels = ["Cat", "Dog"]).plot(cmap = 'Reds')
     plt.show()
 
 
